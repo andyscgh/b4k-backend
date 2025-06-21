b913d892b38d2e69a46f33e4278cf7d1b401f427
andyscgh <72704412+andyscgh@users.noreply.github.com>
2025-06-21 13:22:00 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
+const express = require('express');
+const app = express();
+
+// другие настройки
+
+app.get('/ping', (req, res) => {
+  res.send('pong');
+});
+
+app.listen(3000, () => {
+  console.log('Server is running');
+});
 // Express backend entry with admin features
