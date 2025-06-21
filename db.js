fd676d79aa69ff72d26885b7d5d6cf578b07f878
andyscgh <72704412+andyscgh@users.noreply.github.com>
2025-06-21 16:30:02 +0300

    Update db.js

--- db.js
+++ db.js
@@ -1,1 +1,12 @@
+const { Pool } = require('pg');
+require('dotenv').config();
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false, // это важно для Railway
+  },
+});
+
+module.exports = pool;
 // SQLite DB setup with employees table and is_admin field
